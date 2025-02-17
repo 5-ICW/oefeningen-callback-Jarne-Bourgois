fb2b42c4932624b5f238cb669da097a7587b985e
jarnebourgois <Jarne.Bourgois@student.tsaam.be>
2025-02-17 12:31:41 +0100

    commit laaste

--- oef2.js
+++ oef2.js
@@ -1,20 +1,18 @@
 /**
- * Schrijf een functie processArray die een array en een callback-functie accepteert. 
+ * Schrijf een functie processArray die een array en een callback-functie accepteert.
  * De callback moet op elk element van de array worden toegepast.
  */
 
 function processArray(arr, callback) {
- return callback(arr)
- 
-  
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = callback(arr[i]);
+  }
+  return arr;
 }
 
 // Verwacht resultaat: [2, 4, 6, 8, 10]
 const uitkomst = processArray([1, 2, 3, 4, 5], function (num) {
-  for (let i = 0; i < num.length; i++) {
-    num[i] *= 2
-  }
-  return num ;
+  return num * 2;
 });
 
 console.log(uitkomst);
