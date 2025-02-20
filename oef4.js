908344b859bfb00f358fd15db4f4e74625177ff5
jarnebourgois <Jarne.Bourgois@student.tsaam.be>
2025-02-20 11:00:51 +0100

    f

--- oef4.js
+++ oef4.js
@@ -1,13 +1,13 @@
 /**
- * Maak een functie doubleNumber die een getal verdubbelt 
+ * Maak een functie doubleNumber die een getal verdubbelt
  * met behulp van een callback.
  */
 
 function doubleNumber(num, callback) {
-  callback(num)
+  callback(num);
 }
 
 // Verwacht resultaat: 10
 doubleNumber(5, function (res) {
-  console.log(res*2);
+  console.log(res * 2);
 });
